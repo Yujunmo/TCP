a0feccf7503357783d59253253fc999b4ee38ca3
Yu <whitesky118@gmail.com>
2020-03-07 01:49:29 +0900

    echo client revisied

--- echo_client.c
+++ echo_client.c
@@ -12,6 +12,8 @@ int main(int argc, char* argv[]){
 	struct sockaddr_in serv_addr;
 	char message[30];
 	int str_len;
+	int recv_len;
+	int recv_cnt;
 
 	if(argc!=3){
 		printf("Usage : %s <IP> <port>\n",argv[0]);
@@ -32,17 +34,22 @@ int main(int argc, char* argv[]){
 	else
 		puts("connected ........");
 
-
+	
 	while(1){
 		fputs("input message(Q to quit) : ", stdout);
 		fgets(message, BUF_SIZE, stdin);
 
-		if(!strcmp(message, "q\n")|| !strcmp(message,"Q\n"))
-				break;
-
-		write(sock, message, strlen(message));
-		str_len= read(sock, message, BUF_SIZE-1);
-		message[str_len]=0;
+		if(!strcmp(message, "q\n")|| !strcmp(message,"Q\n")){break;}
+		str_len= write(sock, message, strlen(message));
+		recv_len =0;
+		
+		while(recv_len<str_len){
+			recv_cnt=read(sock, &message[recv_len], BUF_SIZE-1);
+			if(recv_cnt==-1)
+				error_handling("read() error!");
+			recv_len += recv_cnt;
+		}
+		message[recv_len]=0;
 		printf("message from server :  %s", message);
 	}
 	close(sock);
